97c855f780a2bfb354a5a6a7f4a11fae2fa40785
Shiva Prasad <shivaprasad14083@gmail.com>
2024-10-25 12:59:54 +0530

    Added the remaining solutions for wee 0 cyberchase assignment

---
+++ week_0/13.sql
@@ -0,0 +1,3 @@
+SELECT "season" , "title" , "air_date"
+FROM "episodes"
+WHERE "season" BETWEEN 3 AND 5;
---
+++ week_0/8.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(*) FROM "episodes"
+wHERE "air_date" BETWEEN '2018-01-01' AND '2023-12-31';
